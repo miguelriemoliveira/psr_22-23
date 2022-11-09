ef6023a31b20225bae1f70d638e07d1f8b19f610
Miguel Oliveira <m.riem.oliveira@gmail.com>
2022-11-09 16:55:16 +0000

    Examples from the classes.

---
+++ Parte08/Ex2/dog_lib.py
@@ -0,0 +1,18 @@
+from colorama import Fore, Style
+
+
+class Dog:
+    def __init__(self, name, color, age, owner):
+        self.name, self.color, self.age = name, color, age
+        self.owner = owner
+        self.brothers = []  # no brothers for now
+
+    def addBrother(self, name):
+        self.brothers.append(name)
+
+    def __str__(self):
+        return 'name: ' + Fore.RED + str(self.name) + Fore.RESET + \
+               ', age: ' + Fore.RED + str(self.age) + Fore.RESET +\
+               ', color: ' + Fore.RED + str(self.color) + Fore.RESET +\
+               ', owner: ' + Fore.BLUE + str(self.owner) + Fore.RESET +\
+               ', brothers: ' + Fore.BLUE + str(self.brothers) + Style.RESET_ALL
